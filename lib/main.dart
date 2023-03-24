4bbdf5fb815275580ffe4ff3231e66fdf309ba8d
wogood Afifi <wa1352@fayoum.edu.eg>
2023-03-24 23:54:32 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Scaffold(
+      appBar:AppBar(
+        title:Text('First Application'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      backgroundColor: Colors.greenAccent,
+      body:Center(
+        child:Text(
+          'wogood elsayed Afifi')
+        )),
+  ),
+  );
+}
+
